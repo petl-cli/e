cab333da4fb77f22d2bf22ffe4697bc5a6f902e5
rishimantri795 <106453647+rishimantri795@users.noreply.github.com>
2026-05-03 17:52:46 -0400

    release: v0.1.1

--- commands/root.go
+++ commands/root.go
@@ -26,7 +26,7 @@ const _defaultBaseURL = "https://api.spotify.com/v1"
 var rootCmd = &cobra.Command{
 	Use:           "spotify-web-api",
 	Short:         "You can use Spotify's Web API to discover music and podcasts, manage your Spotify library, control audio playback, and much more. Browse our available Web API endpoints using the sidebar at left, or via the navigation bar on top of this page on smaller screens.  In order to make successful Web API requests your app will need a valid access token. One can be obtained through <a href=\"https://developer.spotify.com/documentation/general/guides/authorization-guide/\">OAuth 2.0</a>.  The base URI for all Web API requests is `https://api.spotify.com/v1`.  Need help? See our <a href=\"https://developer.spotify.com/documentation/web-api/guides/\">Web API guides</a> for more information, or visit the <a href=\"https://community.spotify.com/t5/Spotify-for-Developers/bd-p/Spotify_Developer\">Spotify for Developers community forum</a> to ask questions and connect with other developers. ",
-	Version:       "0.1.0",
+	Version:       "0.1.1",
 	SilenceErrors: true, // Execute() handles error printing so Cobra doesn't double-print
 	SilenceUsage:  true, // Don't dump usage on every RunE error
 }
@@ -168,7 +168,7 @@ func withTelemetry(fn func(*cobra.Command, []string) error) func(*cobra.Command,
 		evt := telemetry.Event{
 			CLIID:        _telemetryToken,
 			CLIName:      "spotify-web-api",
-			CLIVersion:   "0.1.0",
+			CLIVersion:   "0.1.1",
 			Command:      cmd.CommandPath(),
 			CallerType:   string(caller.Type),
 			AgentType:    caller.AgentType,
